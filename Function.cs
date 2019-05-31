66a98e81ced32b41c8fb648bfb5943fae9cdec0a
Steven Pears <steven.pears@live.co.uk>
2019-05-31 10:52:50 +0100

    Add Initial Load

---
+++ Function.cs
@@ -0,0 +1,18 @@
+﻿using Alexa.NET.Request;
+using Alexa.NET.Response;
+using Amazon.Lambda.Core;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+#$if$ ($targetframeworkversion$ >= 3.5)using System.Linq;
+$endif$using System.Text;
+
+namespace $safeprojectname$
+{
+	public class Function
+    {
+        public SkillResponse FunctionHandler(SkillRequest input, ILambdaContext context)
+        {
+            return ResponseBuilder.Tell("Response from your project");
+        }
+    }
+}
